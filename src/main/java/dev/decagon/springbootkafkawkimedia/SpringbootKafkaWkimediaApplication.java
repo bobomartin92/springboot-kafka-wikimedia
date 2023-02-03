5f483cd9fa7096facf9b79ac60a451a4c9e593af
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2023-02-03 09:41:30 +0100

    initial commit

---
+++ src/main/java/dev/decagon/springbootkafkawkimedia/SpringbootKafkaWkimediaApplication.java
@@ -0,0 +1,13 @@
+package dev.decagon.springbootkafkawkimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaWkimediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaWkimediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/decagon/springbootkafkawkimedia/SpringbootKafkaWkimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.decagon.springbootkafkawkimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaWkimediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
