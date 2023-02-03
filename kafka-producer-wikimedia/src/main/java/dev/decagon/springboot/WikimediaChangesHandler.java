56daa8e289878ae8ae6721dd62368ab387d8eec6
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2023-02-03 16:03:28 +0100

    implemented wikimedia producer and created a topic

---
+++ kafka-producer-wikimedia/src/main/java/dev/decagon/springboot/WikimediaChangesHandler.java
@@ -0,0 +1,42 @@
+package dev.decagon.springboot;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@AllArgsConstructor
+public class WikimediaChangesHandler implements EventHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesHandler.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private String topic;
+    @Override
+    public void onOpen() throws Exception {
+
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+        LOGGER.info(String.format("event data -> %s", messageEvent));
+        kafkaTemplate.send(topic, messageEvent.getData());
+    }
+
+    @Override
+    public void onComment(String s) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+}
